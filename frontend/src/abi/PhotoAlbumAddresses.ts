784e554be322469ace6ce8e23c7b67e8b59bbe09
logernyk <logernyk@proton.me>
2025-10-27 19:29:55 +0100

    Add PhotoAlbumAddresses.ts

---
+++ frontend/src/abi/PhotoAlbumAddresses.ts
@@ -0,0 +1,7 @@
+export const PhotoAlbumAddresses = {
+  "11155111": {
+    "address": "0xc9AE40B029ad0B858B6F50A1c2931b980f4c877b",
+    "chainId": 11155111,
+    "chainName": "sepolia"
+  }
+} as const;
